171136e21d2220b6811a6d4548c8c0a9c90f6942
charlesx17 <charles071719@gmail.com>
2020-07-11 00:09:49 +0800

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -13,6 +15,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: 'false' }));
 
 const mongoose = require('mongoose');
 
@@ -26,7 +29,10 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database..'));
 
+// Routers //
+
 app.use('/', indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server runnning...');
